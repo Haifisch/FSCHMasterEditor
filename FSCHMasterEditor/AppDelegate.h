5790ecb541878817d5ffab297cd293177c42b760
Dylan Laws <dylan.laws@guardianapp.com>
2022-09-09 15:18:38 -0700

    Initial Commit

---
+++ FSCHMasterEditor/AppDelegate.h
@@ -0,0 +1,14 @@
+//
+//  AppDelegate.h
+//  FSCHMasterEditor
+//
+//  Created by Dylan Laws on 9/9/22.
+//
+
+#import <Cocoa/Cocoa.h>
+
+@interface AppDelegate : NSObject <NSApplicationDelegate>
+
+
+@end
+
---
+++ FSCHMasterEditor/main.m
@@ -0,0 +1,15 @@
+//
+//  main.m
+//  FSCHMasterEditor
+//
+//  Created by Dylan Laws on 9/9/22.
+//
+
+#import <Cocoa/Cocoa.h>
+
+int main(int argc, const char * argv[]) {
+    @autoreleasepool {
+        // Setup code that might create autoreleased objects goes here.
+    }
+    return NSApplicationMain(argc, argv);
+}
